7b9995905a48fa69e2673d1c2ce670d1af577cf5
José Antonio <joantoniorm@gmail.com>
2024-02-19 22:39:29 +0100

    Primer Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Home' title`, () => {
+  it(`should have the 'Homes-Angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Home');
+    expect(app.title).toEqual('Homes-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Home');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Homes-Angular');
   });
 });
